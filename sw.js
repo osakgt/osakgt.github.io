9bbf963ad27aed8cac28db9fc8902c490590457a
osakgt <23040722@hdu.edu.cn>
2026-06-18 10:50:27 +0800

    Site updated: 2026-06-18 10:50:23

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1779428012205";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1781750942831";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
